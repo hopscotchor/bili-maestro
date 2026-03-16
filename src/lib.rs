093f33a77f93d2708ab575e0d8bf5d88dc3335e8
jevan <jevan.dev@outlook.com>
2026-03-17 05:16:18 +0800

    test1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod api;
 pub mod command;
-pub mod config;
 pub mod source;
--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,11 +1,1 @@
-pub mod websocket;
 
-use async_trait::async_trait;
-
-use crate::command::types::DanmakuMessage;
-
-#[async_trait]
-pub trait DanmakuSource: Send {
-    async fn connect(&mut self) -> anyhow::Result<()>;
-    async fn next_message(&mut self) -> anyhow::Result<DanmakuMessage>;
-}
